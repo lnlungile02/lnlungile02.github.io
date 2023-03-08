d47accc8d49a007bb847e756fa3c96b25181c691
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2023-03-08 08:37:08 +0200

    Create products.php

---
+++ products.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Our Products | Ink Supplier Company</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Ink Supplier Company</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="products.php">Products</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<main>
+		<h2>Our Products</h2>
+		<p>Check out our selection of high-quality ink products.</p>
+		
+		<div class="product">
+			<img src="ink1.jpg" alt="Ink product 1">
+			<h3>Ink Product 1</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet nisl at justo suscipit, a venenatis massa sagittis.</p>
+			<p><strong>Price:</strong> $19.99</p>
+			<button>Add to Cart</button>
+		</div>
+		
+		<div class="product">
+			<img src="ink2.jpg" alt="Ink product 2">
+			<h3>Ink Product 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet nisl at justo suscipit, a venenatis massa sagittis.</p>
+			<p><strong>Price:</strong> $24.99</p>
+			<button>Add to Cart</button>
+		</div>
+		
+		<div class="product">
+			<img src="ink3.jpg" alt="Ink product 3">
+			<h3>Ink Product 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet nisl at justo suscipit, a venenatis massa sagittis.</p>
+			<p><strong>Price:</strong> $29.99</p>
+			<button>Add to Cart</button>
+		</div>
+	</main>
+	
+	<footer>
+		<p>&copy; 2023 Ink Supplier Company</p>
+	</footer>
+</body>
+</html>
